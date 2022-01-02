edad28a4ae355286b4f4ef51ca833d0eaa22c999
justinluu8235 <justinluu8235@gmail.com>
2022-01-02 03:01:28 -0800

    CRUD complete for tracker and items

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const {ConstraintTracker, ConstraintItem} = require('./models')
-// const methodOverride = require('method-override');
-// app.use(methodOverride('_method'));
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
 app.get("/constraintTracker" , async (request, response) => {
     try{
@@ -36,6 +36,14 @@ app.post("/constraintTracker", async (request, response) => {
     //parse data into 'group' object    
     let groupArr = [memberNameArr.length];
     for(let i=0; i<memberNameArr.length; i++){
+         //if both fields are empty, skip this one
+         if(memberNameArr[i] === '' && memberCompanyArr[i] === '' ){
+            continue;
+        }
+        //if company is empty, make it N/A
+        else if (memberCompanyArr[i] === '' ){
+            memberCompanyArr[i] = 'N/A';
+        }
         let memberObj = {
             name: memberNameArr[i],
             company: memberCompanyArr[i]
@@ -60,6 +68,86 @@ app.post("/constraintTracker", async (request, response) => {
 
 
 
+//Update an existing tracker
+app.post("/constraintTracker/:id", async (request, response) => {
+    let trackerId = request.params.id;
+  
+    let memberNameArr = request.body.memberName;
+    let memberCompanyArr = request.body.memberCompany;
+
+    //parse data into 'group' object    
+    let groupArr = [memberNameArr.length];
+    for(let i=0; i<memberNameArr.length; i++){
+        //if both fields are empty, skip this one
+        if(memberNameArr[i] === '' && memberCompanyArr[i] === '' ){
+            continue;
+        }
+        //if company is empty, make it N/A
+        else if (memberCompanyArr[i] === '' ){
+            memberCompanyArr[i] = 'N/A';
+        }
+        let memberObj = {
+            name: memberNameArr[i],
+            company: memberCompanyArr[i]
+        }
+        groupArr[i] = memberObj;
+    }
+
+    try{
+       let updatedTracker =  await ConstraintTracker.updateOne({
+            _id: trackerId
+        },{
+            $set: {
+                trackerName: request.body.trackerName,
+                group: groupArr,
+                constraintOpen: 1
+            }
+        });
+
+        response.redirect("http://localhost:3001")
+    }
+    catch(err) {
+        response.status(500).send(error);
+    }
+    
+});
+
+app.delete("/constraintTracker/:id" , async (request, response) => {
+    try{
+
+        let deletedItems = await ConstraintItem.remove({
+            trackerId: request.params.id
+        });
+
+        console.log(deletedItems);
+        
+        let deletedTracker = await ConstraintTracker.remove({
+            _id: request.params.id
+        });
+        console.log(deletedTracker);
+        
+        response.redirect(`http://localhost:3001/`)
+    }
+    catch(error){
+        response.status(500).send(error);
+    }
+});
+
+
+//route to get information for a single constraint item for editing
+app.get("/constraintItem/edit/:id" , async (request, response) => {
+    try{
+        let constraintItem = await ConstraintItem.findOne({
+            _id: request.params.id
+        })
+       response.json({constraintItem})
+    }
+    catch(error){
+        response.status(500).send(error);
+    }
+});
+
+//Route to get all the constraint items for a trakcer
 app.get("/constraintItem/:id" , async (request, response) => {
     
     try{
@@ -67,9 +155,12 @@ app.get("/constraintItem/:id" , async (request, response) => {
             trackerId: request.params.id
         });
 
+        let stats = await getStats(request.params.id);
+
+
         const constraintTracker = await ConstraintTracker.findById(request.params.id);
         let trackerName = constraintTracker.trackerName;
-        response.json({constraintItemArr, trackerName});
+        response.json({constraintItemArr, trackerName, stats});
     }
     catch(error){
         response.status(500).send(error);
@@ -77,8 +168,45 @@ app.get("/constraintItem/:id" , async (request, response) => {
 
 });
 
+
+//Edit an existing constraint item
+app.post("/constraintItem/edit/:id" , async (request, response) => {
+    try{
+        
+        let updatedConstraintItem = await ConstraintItem.updateOne({
+            _id: request.params.id
+
+        },{
+            $set: {
+                driver: request.body.driver,
+                itemName: request.body.itemName,
+                emailSubject: request.body.emailSubject,
+                bICTeam: request.body.bICTeam,
+                bICName: request.body.bICName,
+                description: request.body.description,
+                nextStep: request.body.nextStep,
+                targetDate: request.body.targetDate,
+                priorityLevel: request.body.priorityLevel,
+            }
+        })
+
+        let item = await ConstraintItem.findOne({
+            _id: request.params.id
+        })
+        let trackerId = item.trackerId;
+
+
+        response.redirect(`http://localhost:3001/constraints/${trackerId}`)
+    }
+    catch(error){
+        response.status(500).send(error);
+    }
+
+
+});
+
+//Create a constraint item
 app.post("/constraintItem/:id" , async (request, response) => {
-    console.log('hi');
     try{
         await ConstraintItem.insertMany({
             driver: request.body.driver,
@@ -89,7 +217,7 @@ app.post("/constraintItem/:id" , async (request, response) => {
             description: request.body.description,
             nextStep: request.body.nextStep,
             targetDate: request.body.targetDate,
-            priorityLevel: request.body.targetDate,
+            priorityLevel: request.body.priorityLevel,
             trackerId: request.params.id
         })
 
@@ -104,13 +232,98 @@ app.post("/constraintItem/:id" , async (request, response) => {
 });
 
 
+app.delete("/constraintItem/:id" , async (request, response) => {
+    try{
+
+        let item = await ConstraintItem.findOne({
+            _id: request.params.id
+        })
+        let trackerId = item.trackerId;
 
 
+        
+        let deletedItem = await ConstraintItem.remove({
+            _id: request.params.id
+        });
+        console.log(deletedItem);
+        response.redirect(`http://localhost:3001/constraints/${trackerId}`)
+    }
+    catch(error){
+        response.status(500).send(error);
+    }
+});
+
+async function getStats(trackerID){
+    let stats = {};
+    let urgentCount = await ConstraintItem.find({
+        trackerId: trackerID,
+        priorityLevel: "Urgent"
+    }).count();
+    stats.urgentCount = urgentCount;
+    let highCount = await ConstraintItem.find({
+        trackerId: trackerID,
+        priorityLevel: "High"
+    }).count();
+    stats.highCount = highCount;
+
+    let mediumCount = await ConstraintItem.find({
+        trackerId: trackerID,
+        priorityLevel: "Medium"
+    }).count();
+    stats.mediumCount = mediumCount;
+
+    let lowCount = await ConstraintItem.find({
+        trackerId: trackerID,
+        priorityLevel: "Low"
+    }).count();
+    stats.lowCount = lowCount;
+
+    let designTeamCount = await ConstraintItem.find({
+        trackerId: trackerID,
+        bICTeam: "Design Team"
+    }).count();
+    stats.designTeamCount = designTeamCount;
+
+    let gcCount = await ConstraintItem.find({
+        trackerId: trackerID,
+        bICTeam: "General Contractor"
+    }).count();
+    stats.gcCount = gcCount;
+
+    let tradePartnerCount = await ConstraintItem.find({
+        trackerId: trackerID,
+        bICTeam: "Trade Partner"
+    }).count();
+    stats.tradePartnerCount = tradePartnerCount;
+
+    let ownerCount = await ConstraintItem.find({
+        trackerId: trackerID,
+        bICTeam: "Owner"
+    }).count();
+    stats.ownerCount = ownerCount;
 
 
+    let items = await ConstraintItem.find({
+        trackerId: trackerID,
+    })
+    let driverObj = {};
+    for(let i=0; i<items.length; i++){
+        let driver = items[i].driver;
+        if(driver == ''){
+            continue;
+        }
 
+        if(driverObj[driver] == null){
+            driverObj[driver] = 1;
+        }
+        else{
+            driverObj[driver] ++;
+        }
+    }
+    stats.driverObj = driverObj;
 
-
+    return stats;
+}
 
 app.listen(3000, () =>{
     console.log("Server is running at port 3000");
