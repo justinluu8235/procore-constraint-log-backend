9e79985f8d4a1f2902c20f56a6336cf58b7f1467
justinluu8235 <justinluu8235@gmail.com>
2022-01-17 16:19:34 -0800

    allow origin

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.use(methodOverride('_method'));
 
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 app.get("/constraintTracker" , async (request, response) => {
     try{
         const constraintTrackerArray = await ConstraintTracker.find({});
